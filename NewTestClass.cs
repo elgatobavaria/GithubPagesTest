c56770da622c67cc925e5443d5cd007046e04d4f
virszv <mathias.zviedris@vector.com>
2022-07-21 11:31:05 +0200

    test

---
+++ NewTestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjekt
+{
+    public class NewTestClass
+    {
+        public NewTestClass()
+        {
+
+        }
+
+        public void printBlaBla()
+        { 
+            Console.WriteLine("BlaBla");
+            }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          NewTestClass tc = new NewTestClass();
+          tc.printBlaBla();
+          Console.ReadKey();
+
+        }
+    }
+}
